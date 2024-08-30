fe4638bbfebe150131285b42a33ad43fb46d1405
= <=>
2024-08-30 12:53:33 +0530

    first commit

---
+++ dpro/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def dashboard(request):
+    return render(request,"index.html")
+
+def calculator(request):
+    return render(request,"cal.html")
+
+def card(request):
+    return render(request,"card.html")
+
+def time_table(request):
+    return render(request,"time_table.html")
